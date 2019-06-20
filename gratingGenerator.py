5b1cd551ced37f8ce2c24a4fc086f53f98802952
fcorapi <fcorapi@uwaterloo.ca>
2019-06-20 18:22:28 -0400

    Calibration Gratings Addition

--- gratingGenerator.py
+++ gratingGenerator.py
@@ -11,15 +11,19 @@ from PIL import Image
 #******************USER INPUTS***************************
 xDim = 912  #Length of DMD
 yDim = 1140 #Width of DMD
-dir = 'C:\Users\Franky\Desktop\UofT Summer 2019\Gratings\ ' #Directory to save png image file
-dirBMP = 'C:\Users\Franky\Desktop\UofT Summer 2019\Gratings\BMP\ ' #Directory to save converted bmp image file
-filename = 'grating1' #Filename for the grating
+dir = 'C:\Users\Franky\Desktop\UofT Summer 2019\Gratings\CalibrationGratings\PNG\ ' #Directory to save png image file
+dirBMP = 'C:\Users\Franky\Desktop\UofT Summer 2019\Gratings\CalibrationGratings\BMP\ ' #Directory to save converted bmp image file
+dirBMP1bit = 'C:\Users\Franky\Desktop\UofT Summer 2019\Gratings\CalibrationGratings\BMP_8bit\ '
+filename = 'CG' #Filename for the grating
 ext1 = '.png' #image extension 1
 ext2 = '.bmp' #image extension 2
-ext3 = '_1bit.bmp'
+ext3 = '_8bit.bmp'
 
-period = 10 #period of grating in pixels
+period = 2 #period of grating in pixels
 angle = 0  #grating angle in radians
+#NX = 25 #Number of patches in X-direction
+#NY = 30 #Number of patches in Y-direction
+r = 57 #radius of calibration patch
 #*******************************************************
 
 
@@ -31,17 +35,27 @@ def grating(x,y,p,theta):
     g = np.round(0.5*(1+np.cos((2*np.pi/p)*(x*np.cos(theta)+y*np.sin(theta)))))
     return g
 
-gTest = grating(xMesh, yMesh,period,angle)
-print np.shape(xMesh)
-print gTest
-plt.imshow(gTest, cmap='gray')
-plt.show()
 
-img.imsave(dir+filename+ext1, gTest, format = 'png', cmap='gray')
+for xPos in np.arange(r,xDim+r,2*r):
+    for yPos in np.arange(r,yDim+r, 2*r):
+        filenameNumber = '_X_'+str(xPos)+'_Y_'+str(yPos)
+        g1 = grating(xMesh, yMesh, period, angle)
+        g2 = grating(xMesh, yMesh, period, angle)
+        for loopx in range(0,xDim-1):
+            for loopy in range(0,yDim-1):
+                if (loopx - xPos)**2 +(loopy-yPos)**2 > r**2:
+                    g1[loopy, loopx] = 0
+                if (loopx - ((xDim-2*r)/2))**2 + (loopy-((yDim-2*r)/2))**2 > r**2:
+                    g2[loopy,loopx] = 0
+        #plt.imshow(g, cmap='gray')
+        #plt.show()
+        g = g1+g2
+        img.imsave(dir+filename+filenameNumber+ext1, g, format = 'png', cmap='gray')
 
-pngImage = Image.open(dir+filename+ext1)
-pngImage.save(dirBMP+filename+ext2)
+        pngImage = Image.open(dir+filename+filenameNumber+ext1)
+        pngImage.save(dirBMP+filename+filenameNumber+ext2)
 
-oldBmp = Image.open(dirBMP+filename+ext2)
-newBmp = oldBmp.convert('1')
-newBmp.save(dirBMP+filename+ext3)
+        oldBmp = Image.open(dirBMP+filename+filenameNumber+ext2)
+        newBmp = oldBmp.convert('L')
+        newBmp.save(dirBMP1bit+filename+filenameNumber+ext3)
+        print 'X = ', xPos, 'Y = ', yPos, ' completed.'
